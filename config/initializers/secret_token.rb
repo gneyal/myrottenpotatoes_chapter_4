1b0976ebd41f3f1a0219f60a80ab2476f024548c
Ubuntu <ubuntu@ip-10-252-89-228.us-west-2.compute.internal>
2012-08-02 07:01:56 +0000

    initial commit - now need to add the show action and view, location in the book 3041

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrottenpotatoes::Application.config.secret_token = 'a4629e394e0565a06a1e9164365de76bd65e29fdd86f8c771c31ced2541160e815fd0a4fd692036a3213dcc77daeab8bd7f4f718c4ebb9e004d1b2b1d157f864'
